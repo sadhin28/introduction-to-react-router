37a52de45c012954b0b0a7dfa1d00df30f1ef66c
sadhin28 <taosifsadhin113@gmail.com>
2025-05-10 22:38:13 +0600

    add footer section

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import {
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
-import Header from './Components/Header/Header';
 const router = createBrowserRouter([
   {
     path: '/',
