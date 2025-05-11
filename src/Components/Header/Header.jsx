9c66723070fae183369baf33635010cdca28c192
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 00:31:26 +0600

    add navlink

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import { Link} from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <div className=" sticky top-0 text-md:2xl font-black border-b-4 border-amber-800 py-5 px-5 bg-amber-100 flex justify-end gap-4 ">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/contact'>Contact Us</NavLink>
+        <NavLink  to='/user'>User</NavLink>
+        <NavLink to='/posts'>Posts</NavLink>
         
-         <Link to='/'>Home</Link>
-         <Link to='/about'>About</Link>
-         <Link to='/contact'>Contact Us</Link>
-         <Link to='/user'>User</Link>
-         <Link to='/posts'>Posts</Link>
         </div>
     );
 };
