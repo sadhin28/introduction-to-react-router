4c8f8ee446e3508e110e44b29eff0e8e168d0758
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 00:09:51 +0600

    add  data form json file

--- src/Components/Postdetails/Postdetails.jsx
+++ src/Components/Postdetails/Postdetails.jsx
@@ -12,7 +12,7 @@ const Postdetails = () => {
         <div className='border-4 p-5 rounded-2xl  text-2xl '>
             <h1 className='text-sm font-black'>Title : {d.title}</h1>
             <p className='text-sm'>Description : {d.body}</p>
-            <button  onClick={handeGoback} className='btn btn mt-4 bg-amber-900 text-white text-sm border-2 p-2 rounded-tl-2xl hover:shadow-2xs hover:bg-black hover:text-white rounded-br-2xl'>Go Back</button>
+            <button  onClick={handeGoback} className='btn mt-4 bg-amber-900 text-white text-sm border-2 p-2 rounded-tl-2xl hover:shadow-2xs hover:bg-black hover:text-white rounded-br-2xl'>Go Back</button>
         </div>
     );
 };
--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -12,7 +12,7 @@ const UserDetails = () => {
         <div className='border-4 p-5 rounded-2xl text-2xl font-black'>
              <h1 className=''>Name: {name}</h1>
              <h2>Email: {email}</h2>
-             <button  onClick={userGoback} className='btn btn mt-4 bg-amber-900 text-white text-sm border-2 p-2 rounded-tl-2xl hover:shadow-2xs hover:bg-black hover:text-white rounded-br-2xl'>Go back</button>
+             <button  onClick={userGoback} className='btn mt-4 bg-amber-900 text-white text-sm border-2 p-2 rounded-tl-2xl hover:shadow-2xs hover:bg-black hover:text-white rounded-br-2xl'>Go back</button>
         </div>
     );
 };
