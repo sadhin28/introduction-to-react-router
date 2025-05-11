6ffe2bde87876a594fc35968bb6460c4db3a280a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-11 22:06:03 +0600

    add post data

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Post = ({data}) => {
+    const {id,title,body,}=data
+    return (
+        <div className='border-4 p-5 rounded-2xl  text-xl font-black'>
+            <h1>Title: {title}</h1>
+            <p className='text-sm text-justify'>{body}</p>
+            <Link className='btn mt-4 bg-amber-900 text-white text-sm border-2 p-2 rounded-tl-2xl hover:shadow-2xs hover:bg-black hover:text-white rounded-br-2xl' to={`/data/${id}`}>Post Details</Link>
+        </div>
+    );
+};
+
+export default Post;
--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Post from "../Post/Post";
 
 const Posts = () => {
     const posts = useLoaderData();
@@ -6,6 +7,11 @@ const Posts = () => {
     
         <div>
         <h2>Post:{posts.length} </h2>
+         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+             {
+            posts.map(data=><Post key={data.id} data={data}></Post>)
+           }
+         </div>
         </div>
     );
 };
