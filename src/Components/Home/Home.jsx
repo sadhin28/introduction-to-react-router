193ea737f2a053dc40d764eacffac5824089dc75
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 01:07:47 +0600

    add navigation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 
 const Home = () => {
+    const navigation =useNavigation();
     return (
         <div>
            <Header></Header><br /><br />
-           <Outlet></Outlet>
+           <div className="">
+            {
+              navigation.state === 'loading' ?
+               <div className="w-50  mx-auto"><span className=" w-20 loading loading-bars "></span></div>
+               :<Outlet></Outlet>
+           }
+           </div>
+         
            
         </div>
     );
