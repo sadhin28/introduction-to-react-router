67dbdfa09a98004b7c211190b29dd288f9af318a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-11 23:38:30 +0600

    add data from posts

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -6,7 +6,7 @@ const Posts = () => {
     return (
     
         <div>
-        <h2>Post:{posts.length} </h2>
+        <h2 className="text-center py-10 font-bold text-2xl md:text-4xl">Post:{posts.length} </h2>
          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 ">
              {
             posts.map(datas=><Post key={datas.id} datas={datas}></Post>)
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -6,7 +6,7 @@ const User = () => {
      const data = useLoaderData()
     return (
         <div className=''>
-            <h1>Our Users: {data.length}</h1>
+            <h1 className='text-center py-10 font-bold text-2xl md:text-4xl'>Our Users: {data.length}</h1>
            <div className=' grid md:grid-cols-2 lg:grid-cols-3 gap-5 '>
               {
                 data.map(userData=><UserDataCard key={data.is} UserData={userData}></UserDataCard>)
