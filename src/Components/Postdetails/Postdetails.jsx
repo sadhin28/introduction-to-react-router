428e492bd818cb6badfc894260645415580838a0
sadhin28 <taosifsadhin113@gmail.com>
2025-05-11 23:51:10 +0600

    add go back butto for posts

--- src/Components/Postdetails/Postdetails.jsx
+++ src/Components/Postdetails/Postdetails.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const Postdetails = () => {
 
    const d =useLoaderData();
+   const navigate = useNavigate();
+      const handeGoback =()=>{
+       navigate(-1)
+   }
     return (
         <div className='border-4 p-5 rounded-2xl  text-2xl '>
             <h1 className='text-sm font-black'>Title : {d.title}</h1>
             <p className='text-sm'>Description : {d.body}</p>
+            <button onClick={handeGoback} className='btn'>Go Back</button>
         </div>
     );
 };
