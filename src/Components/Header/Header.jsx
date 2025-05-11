22e3a4c1d12b3de98ecf0bb33b021a1907ee5386
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 00:46:54 +0600

    add navlink data

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link, NavLink} from "react-router-dom";
-
+import {  NavLink} from "react-router-dom";
+import './Header.css'
 const Header = () => {
     return (
-        <div className=" sticky top-0 text-md:2xl font-black border-b-4 border-amber-800 py-5 px-5 bg-amber-100 flex justify-end gap-4 ">
+        <nav className=" sticky top-0 text-md:2xl font-black border-b-4 border-amber-800 py-5 px-5 bg-amber-100 flex justify-end gap-4 ">
         <NavLink to="/">Home</NavLink>
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/contact'>Contact Us</NavLink>
         <NavLink  to='/user'>User</NavLink>
         <NavLink to='/posts'>Posts</NavLink>
         
-        </div>
+        </nav>
     );
 };
 
